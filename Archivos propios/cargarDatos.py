f9a7d73f4557f23d9c3ffcc1b80bb128447ca3e1
FacuOrtiz1 <facu.ortiz0095@gmail.com>
2023-05-03 20:01:20 -0300

    Commit desde notebook: regresión lineal múltiple, eliminación hacía atrás

--- Archivos propios/cargarDatos.py
+++ Archivos propios/cargarDatos.py
@@ -21,13 +21,14 @@ from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split # La función train_test_split es la función que nos va a permitir dividir el dataset. 
  
 # Importar el dataset
-dataset = pd.read_csv('D:/FACU/machinelearning-az/datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data.csv')
+# dataset = pd.read_csv('D:/FACU/machinelearning-az/datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data.csv')
+dataset = pd.read_csv('C:/Users/Faaor/OneDrive/Escritorio/Curso ML/machinelearning-az/datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data.csv')
 
 X = dataset.iloc[:,:-1].values  # Obtengo los valores de dataset de todas las columnas a excepción de la última.
 Y = dataset.iloc[:,-1].values
 
 # Tratamiento de los NAs
-imputer = SimpleImputer(missing_values = pd.NA, strategy = "mean")
+imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
 imputer.fit(X[:,1:3]) # Le indicamos que valores con los que va a ajustar
 X[:,1:3] = imputer.transform(X[:,1:3]) # Se genera la transformación. Se podría usar fit en otro conjunto de datos y aplicarco sobre X
 
@@ -50,15 +51,15 @@ X = np.array(ct.fit_transform(X), dtype=np.float) # Aplico la transformación cr
 
 
 
-#Regla general: Si las variables NO tiene una cardinalidad u orden, tengo que usar variables dummmy. Si la tienen no las uso. 
+# #Regla general: Si las variables NO tiene una cardinalidad u orden, tengo que usar variables dummmy. Si la tienen no las uso. 
 
 
-# División del dataset en conjunto de entrenamiento y testing
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=0) # Se destina el 20% a test y 80% a train.
+# # División del dataset en conjunto de entrenamiento y testing
+# X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=0) # Se destina el 20% a test y 80% a train.
 
 
-# Proceso de escalado de variables
-# Creamos primero el escalador
-sc_X = preprocessing.StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+# # Proceso de escalado de variables
+# # Creamos primero el escalador
+# sc_X = preprocessing.StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.fit_transform(X_test)
--- Archivos propios/regresiónMultiple.py
+++ Archivos propios/regresiónMultiple.py
@@ -13,16 +13,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
  
 # Importar el Data set
 dataset = pd.read_csv("C:/Users/Faaor/OneDrive/Escritorio/Curso ML/machinelearning-az/datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups.csv")
+# Defino mi matríz  de caracteristicas. X seria la matriz de caracteristicas e Y lo que quiero predecir. 
 X = dataset.iloc[:, :-1].values  
 Y = dataset.iloc[:, -1:].values 
 
+
+
+#No es necesario aplicar LabelEnconder para crear las variables dummy.
+transformer = [('one_hot_encoder', OneHotEncoder(categories='auto'), [-1])] # Creo el transformador de mi columna. 
+ct = ColumnTransformer(transformer, remainder='passthrough') # Creo instancia de la clase. 
+X_t = np.array(ct.fit_transform(X), dtype=np.float)
+
+#Evitar la trampa de las variables ficticias. 
+X_t = X_t[:,1:]
 # Interesante: No se puede aplicar la regresión si el conjunto X no es un array. Y no debe necesariametne ser uno vector
 
 # Dividir el data set en conjunto de entrenamiento y en conjunto de testing
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size = 1/3, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X_t, Y,test_size = .2, random_state = 0)
+
+# Ajustar el modelo de regresión lineal múltiple con el conjunto de entrenamiento
+regretion = LinearRegression()
+regretion.fit(X_train, Y_train)
+
+#Predicción de los resultados en el conjunto de testing. Para esto vamos a usar los datos de testing
+y_pred = regretion.predict(X_test)
