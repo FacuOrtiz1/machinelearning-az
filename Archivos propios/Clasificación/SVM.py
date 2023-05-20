0d239358c91e992a24ae3fcbe5fe7a058d6d14d8
FacuOrtiz1 <facu.ortiz0095@gmail.com>
2023-05-20 18:30:56 -0300

    Hasta clasificación por Bosques Aleatorios

---
+++ Archivos propios/Clasificación/SVM.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 11:28:59 2023
+
+@author: Equipo
+"""
+
+# SVM
+# ----- Preprocesado de información -----
+# Importamos librerias
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+# Importamos el dataset
+dataset = pd.read_csv("D:/FACU/machinelearning-az/datasets/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/Social_Network_Ads.csv")
+X = dataset.iloc[:,2:-1].values
+Y = dataset.iloc[:,-1].values
+
+#Dividir el dataset en conjunto de entrenamiento y conjunto de testing
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+
+# Escalado de variables
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Como lo que quiero predecir es un 0 o un 1, la información ya viene correctamente catalogada en ese vector de entrenamiento y test. Entonces no hace falta
+
+# ----- Ajuste del modelo de SVM en el modelo de entrenamiento -----
+classifier = SVC(kernel="rbf",random_state=0) # p = 2 distancia euclidea
+classifier.fit(X_train,Y_train)
+
+# Predicción de los resultados con el conjunto de testing
+Y_pred = classifier.predict(X_test)
+
+# Matriz de confusión
+cm = confusion_matrix(Y_test, Y_pred)
+
+# Representación gráfica de los resultados del algoritmo en el Conjunto de Entrenamiento
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Conjunto de Entrenamiento)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
+
+# Representación gráfica de los resultados del algoritmo en el Conjunto de Test
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Conjunto de Entrenamiento)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
+
+
+
+
+
